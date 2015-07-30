ae10389d70a3a050259bccb8b5098519b1413ef2
Valentin Kuznetsov <vkuznet@gmail.com>
2015-07-30 18:50:17 +0100

    Add first draft of web page UI; add static handlers for css/js/images

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "web"
-import "flag"
+import (
+	"flag"
+	"os"
+	"path/filepath"
+	"web"
+)
 
 func main() {
-	var port string
+	var port, tdir, tcss, tjs, timg string
 	flag.StringVar(&port, "port", "8212", "DAS server port number")
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	flag.StringVar(&tdir, "tmpl-dir",
+		filepath.Join(cwd, "src/templates"), "Template directory")
+	flag.StringVar(&tcss, "css-dir",
+		filepath.Join(cwd, "src/css"), "CSS directory")
+	flag.StringVar(&tjs, "js-dir",
+		filepath.Join(cwd, "src/js"), "JS directory")
+	flag.StringVar(&timg, "img-dir",
+		filepath.Join(cwd, "src/images"), "Image directory")
 	flag.Parse()
-	web.Server(port)
+	web.Server(port, tdir, tcss, tjs, timg)
 }
--- src/web/server.go
+++ src/web/server.go
@@ -12,14 +12,18 @@
 package web
 
 import (
+	"bytes"
 	"das"
 	"dasmaps"
 	"dasql"
-	//     "html/template"
 	"encoding/json"
+	"html/template"
 	"log"
+	"mongo"
 	"net/http"
+	"path/filepath"
 	"strconv"
+	"time"
 )
 
 // profiler
@@ -27,6 +31,28 @@ import _ "net/http/pprof"
 
 // global dasmaps
 var _dasmaps dasmaps.DASMaps
+var _tdir, _tcss, _tjs, _timg string
+
+// consume list of templates and release their full path counterparts
+func fileNames(tdir string, filenames ...string) []string {
+	flist := []string{}
+	for _, fname := range filenames {
+		flist = append(flist, filepath.Join(tdir, fname))
+	}
+	return flist
+}
+
+// parse template with given data
+func parseTmpl(tdir, tmpl string, data interface{}) string {
+	buf := new(bytes.Buffer)
+	filenames := fileNames(tdir, tmpl)
+	t := template.Must(template.ParseFiles(filenames...))
+	err := t.Execute(buf, data)
+	if err != nil {
+		panic(err)
+	}
+	return buf.String()
+}
 
 func processRequest(dasquery dasql.DASQuery, pid string, idx, limit int) map[string]interface{} {
 	response := make(map[string]interface{})
@@ -53,19 +79,8 @@ func processRequest(dasquery dasql.DASQuery, pid string, idx, limit int) map[str
 /*
  * RequestHandler is used by web server to handle incoming requests
  */
-
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.FormValue("input")
-	dasquery := dasql.Parse(query)
-	log.Println(dasquery)
-
-	pid := r.FormValue("pid")
-	if pid == "" {
-		pid = dasquery.Qhash
-	}
-	if len(pid) != 32 {
-		http.Error(w, "DAS query pid is not valid", http.StatusInternalServerError)
-	}
 	limit, err := strconv.Atoi(r.FormValue("limit"))
 	if err != nil {
 		limit = 10
@@ -75,42 +90,66 @@ func RequestHandler(w http.ResponseWriter, r *http.Request) {
 		idx = 0
 	}
 	path := r.URL.Path
-	//     response := make(map[string]interface{})
-
-	// Remove expire records from cache
-	das.RemoveExpired(dasquery.Qhash)
 
 	// process requests based on the path
+	tmplData := map[string]interface{}{}
+	tmplData["Base"] = "das"
+	tmplData["Time"] = time.Now()
+	tmplData["Views"] = []string{"list", "plain", "table", "json", "xml"}
+	tmplData["DBSes"] = []string{"prod/global", "prod/phys01", "prod/phys02", "prod/phys03", "prod/caf"}
 	if path == "/das" {
-		log.Println("Process /das", query, limit, idx)
-	} else if path == "/das/request" {
-		response := processRequest(dasquery, pid, idx, limit)
-		log.Println("/das/request", response)
-		// put response on a web
-	} else if path == "/das/cache" {
-		response := processRequest(dasquery, pid, idx, limit)
-		status := response["status"]
-		if status != "ok" {
-			w.Write([]byte(response["pid"].(string)))
-			return
-		}
-		js, err := json.Marshal(&response)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+		tmpl := "top.tmpl"
+		top_page := parseTmpl(_tdir, tmpl, tmplData)
+		tmpl = "searchform.tmpl"
+		content := parseTmpl(_tdir, tmpl, tmplData)
+		tmpl = "bottom.tmpl"
+		bottom_page := parseTmpl(_tdir, tmpl, tmplData)
+		w.Write([]byte(top_page + content + bottom_page))
 	} else {
-		//         t, _ := template.ParseFiles("src/templates/error.html")
-		//         t.Execute(w, nil)
-		http.Error(w, "Not implemented path", http.StatusInternalServerError)
+		dasquery := dasql.Parse(query)
+		log.Println(dasquery)
+		pid := r.FormValue("pid")
+		if pid == "" {
+			pid = dasquery.Qhash
+		}
+		if len(pid) != 32 {
+			http.Error(w, "DAS query pid is not valid", http.StatusInternalServerError)
+		}
+		// Remove expire records from cache
+		das.RemoveExpired(dasquery.Qhash)
+		// process given query
+		response := processRequest(dasquery, pid, idx, limit)
+		if path == "/das/cache" {
+			status := response["status"]
+			if status != "ok" {
+				w.Write([]byte(response["pid"].(string)))
+				return
+			}
+			js, err := json.Marshal(&response)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+		} else if path == "/das/request" {
+			log.Println("/das/request", response)
+			// put response on a web
+		} else {
+			//         t, _ := template.ParseFiles("src/templates/error.html")
+			//         t.Execute(w, nil)
+			http.Error(w, "Not implemented path", http.StatusInternalServerError)
+		}
 	}
 }
 
 // proxy server. It defines /fetch public interface
-func Server(port string) {
+func Server(port, tdir, tcss, tjs, timg string) {
 	log.Printf("Start server localhost:%s/das", port)
+	_tdir = tdir // location of templates
+	_tcss = tcss // location of css files
+	_tjs = tjs   // location of js files
+	_timg = timg // location of static images
 
 	// load DAS Maps if neccessary
 	if len(_dasmaps.Services()) == 0 {
@@ -119,6 +158,14 @@ func Server(port string) {
 		log.Println("DAS services", _dasmaps.Services())
 	}
 
+	// create all required indecies in das.cache, das.merge collections
+	indexes := []string{"qhash", "das.expire", "das.record"}
+	mongo.CreateIndexes("das", "cache", indexes)
+
+	// assign handlers
+	http.Handle("/das/css/", http.StripPrefix("/das/css/", http.FileServer(http.Dir(_tcss))))
+	http.Handle("/das/js/", http.StripPrefix("/das/js/", http.FileServer(http.Dir(_tjs))))
+	http.Handle("/das/images/", http.StripPrefix("/das/images/", http.FileServer(http.Dir(_timg))))
 	http.HandleFunc("/das/request", RequestHandler)
 	http.HandleFunc("/das/cache", RequestHandler)
 	http.HandleFunc("/das", RequestHandler)
