e5bfb939c3b806a465a812146f5611bd8d419dc5
Valentin Kuznetsov <vkuznet@gmail.com>
2015-08-04 12:30:33 +0200

    Add dataset_info logic; change loadDBSData logic and add printout for unmarshalled error

--- src/services/dbs.go
+++ src/services/dbs.go
@@ -9,21 +9,33 @@ package services
 
 import (
 	"encoding/json"
+	"fmt"
 	"mongo"
 )
 
 // helper function to load DBS data stream
-func loadDBSData(data []byte) []mongo.DASRecord {
+func loadDBSData(api string, data []byte) []mongo.DASRecord {
 	var out []mongo.DASRecord
 	err := json.Unmarshal(data, &out)
 	if err != nil {
-		panic(err)
+		var out2 interface{}
+		json.Unmarshal(data, &out2)
+		fmt.Println("DBS unable to unmarshal the data into DAS record", api, out2)
 	}
 	return out
 }
 
 // Unmarshal Phedex data stream and return DAS records based on api
 func DBSUnmarshal(api string, data []byte) []mongo.DASRecord {
-	records := loadDBSData(data)
+	records := loadDBSData(api, data)
+	var out []mongo.DASRecord
+	if api == "dataset_info" {
+		for _, rec := range records {
+			rec["name"] = rec["dataset"]
+			delete(rec, "dataset")
+			out = append(out, rec)
+		}
+		return out
+	}
 	return records
 }
