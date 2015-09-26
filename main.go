3fa59c2a1dcb531d9d7781ed4a1ac762ffc0a8ab
Valentin Kuznetsov <vkuznet@gmail.com>
2015-09-26 10:34:49 -0400

    Add verbose parameter

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"flag"
+	"utils"
 	"web"
 )
 
 func main() {
 	var port string
 	flag.StringVar(&port, "port", "8212", "DAS server port number")
+	var verbose bool
+	flag.BoolVar(&verbose, "verbose", false, "Verbose mode of DAS server")
 	flag.Parse()
+	utils.VERBOSE = verbose
 	web.Server(port)
 }
