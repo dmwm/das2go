32596ae51384a14ae16a182d47dcd9022696859a
Valentin Kuznetsov <vkuznet@gmail.com>
2015-10-01 14:21:53 -0400

    New module to handle ReqMgr data-service

---
+++ src/services/reqmgr.go
@@ -0,0 +1,63 @@
+/*
+ *
+ * Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+ * Description: ReqMgr module
+ * Created    : Fri Jun 26 14:25:01 EDT 2015
+ *
+ */
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"mongo"
+)
+
+// helper function to load ReqMgr data stream
+func loadReqMgrData(api string, data []byte) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	if api == "configIDs" {
+		var rec mongo.DASRecord
+		err := json.Unmarshal(data, &rec)
+		if err != nil {
+			msg := fmt.Sprintf("ReqMgr unable to unmarshal the data into DAS record, api=%s, data=%s, error=%v", api, string(data), err)
+			panic(msg)
+		}
+		out = append(out, rec)
+	} else {
+		err := json.Unmarshal(data, &out)
+		if err != nil {
+			msg := fmt.Sprintf("ReqMgr unable to unmarshal the data into DAS record, api=%s, data=%s, error=%v", api, string(data), err)
+			panic(msg)
+		}
+	}
+	return out
+}
+
+// Unmarshal ReqMgr data stream and return DAS records based on api
+func ReqMgrUnmarshal(api string, data []byte) []mongo.DASRecord {
+	records := loadReqMgrData(api, data)
+	var out []mongo.DASRecord
+	if api == "inputdataset" {
+		for _, rec := range records {
+			val := rec["InputDatasets"]
+			if val != nil {
+				datasets := val.([]string)
+				rec["name"] = datasets[0]
+			}
+			out = append(out, rec)
+		}
+		return out
+	} else if api == "configIDs" {
+		for _, rec := range records {
+			for key, val := range rec {
+				crec := make(mongo.DASRecord)
+				crec["request_name"] = key
+				crec["config_files"] = val
+				out = append(out, crec)
+			}
+		}
+		return out
+	}
+	return records
+}
