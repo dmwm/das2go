d72166c726ec1058c3f1b1186c784d243392faa2
Valentin Kuznetsov <vkuznet@gmail.com>
2015-10-01 17:49:58 -0400

    Add conddb unmarshaling

--- src/services/services.go
+++ src/services/services.go
@@ -55,6 +55,8 @@ func Unmarshal(system, api string, data []byte, notations []mongo.DASRecord) []m
 		out = ReqMgrUnmarshal(api, data)
 	case system == "mcm":
 		out = McMUnmarshal(api, data)
+	case system == "conddb":
+		out = CondDBUnmarshal(api, data)
 	}
 	return remap(api, out, notations)
 }
