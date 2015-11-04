b387ecc7c2e6fad170ef239f4bc418b6da5a0a5e
Valentin Kuznetsov <vkuznet@gmail.com>
2015-11-03 19:52:57 -0500

    Change verbose option to be int instead of bool

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	flag.StringVar(&afile, "afile", "", "DAS authentication key file")
 	var port string
 	flag.StringVar(&port, "port", "8212", "DAS server port number")
-	var verbose bool
-	flag.BoolVar(&verbose, "verbose", false, "Verbose mode of DAS server")
+	var verbose int
+	flag.IntVar(&verbose, "verbose", 0, "Verbose level, support 0,1,2")
 	flag.Parse()
 	utils.VERBOSE = verbose
 	web.Server(port, afile)
