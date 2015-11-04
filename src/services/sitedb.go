ec48daff61d8ba7f671a335e25e0d461fdaf8b5b
Valentin Kuznetsov <vkuznet@gmail.com>
2015-11-04 09:44:21 -0500

    Implement sitedb local APIs

--- src/services/sitedb.go
+++ src/services/sitedb.go
@@ -10,7 +10,11 @@ package services
 import (
 	"encoding/json"
 	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 	"mongo"
+	"strings"
+	"utils"
 )
 
 // helper function to load SiteDB data stream
@@ -20,7 +24,9 @@ func loadSiteDBData(api string, data []byte) []mongo.DASRecord {
 	err := json.Unmarshal(data, &rec)
 	if err != nil {
 		msg := fmt.Sprintf("SiteDB unable to unmarshal the data into DAS record, api=%s, data=%s, error=%v", api, string(data), err)
-		panic(msg)
+		//         panic(msg)
+		fmt.Println(msg)
+		return out
 	}
 	desc := rec["desc"].(map[string]interface{})
 	headers := desc["columns"].([]interface{})
@@ -44,4 +50,110 @@ func loadSiteDBData(api string, data []byte) []mongo.DASRecord {
 func SiteDBUnmarshal(api string, data []byte) []mongo.DASRecord {
 	records := loadSiteDBData(api, data)
 	return records
+}
+
+/*
+ * Local SiteDB APIs
+ */
+func getSiteDBData(api string) []mongo.DASRecord {
+	furl := fmt.Sprintf("%s/%s", sitedbUrl(), api)
+	response := utils.FetchResponse(furl, "")
+	if response.Error == nil {
+		records := loadSiteDBData(api, response.Data)
+		return records
+	} else {
+		log.Println(fmt.Sprintf("DAS ERROR, SiteDB API=%s, error=%s", api, response.Error))
+	}
+	var out []mongo.DASRecord
+	return out
+}
+
+// site-names
+func (LocalAPIs) L_sitedb2_site_names(spec bson.M) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	api := "site-names"
+	site := spec["site"].(string)
+	records := getSiteDBData(api)
+	for _, r := range records {
+		if r["site_name"].(string) == site {
+			r["name"] = r["site_name"]
+			out = append(out, r)
+		}
+	}
+	return out
+}
+
+// groups
+func (LocalAPIs) L_sitedb2_groups(spec bson.M) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	api := "groups"
+	group := spec["group"].(string)
+	records := getSiteDBData(api)
+	for _, r := range records {
+		if r["name"].(string) == group {
+			out = append(out, r)
+		}
+	}
+	return out
+}
+
+// group_responsibilities
+func (LocalAPIs) L_sitedb2_group_responsibilities(spec bson.M) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	api := "group-responsibilities"
+	group := spec["group"].(string)
+	records := getSiteDBData(api)
+	for _, r := range records {
+		if r["user_group"].(string) == group {
+			r["name"] = r["user_group"]
+			out = append(out, r)
+		}
+	}
+	return out
+}
+
+// people_via_email
+func (LocalAPIs) L_sitedb2_people_via_email(spec bson.M) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	api := "people"
+	user := spec["user"].(string)
+	records := getSiteDBData(api)
+	for _, r := range records {
+		if r["email"].(string) == user {
+			out = append(out, r)
+		}
+	}
+	return out
+}
+
+// people_via_name
+func (LocalAPIs) L_sitedb2_people_via_name(spec bson.M) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	api := "people"
+	user := strings.ToLower(spec["user"].(string))
+	records := getSiteDBData(api)
+	for _, r := range records {
+		username := strings.ToLower(r["username"].(string))
+		forename := strings.ToLower(r["forename"].(string))
+		surname := strings.ToLower(r["surname"].(string))
+		email := strings.ToLower(r["email"].(string))
+		if username == user || forename == user || surname == user || email == user {
+			out = append(out, r)
+		}
+	}
+	return out
+}
+
+// roles
+func (LocalAPIs) L_sitedb2_roles(spec bson.M) []mongo.DASRecord {
+	var out []mongo.DASRecord
+	api := "roles"
+	role := spec["role"].(string)
+	records := getSiteDBData(api)
+	for _, r := range records {
+		if r["title"].(string) == role {
+			out = append(out, r)
+		}
+	}
+	return out
 }
