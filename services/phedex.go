a573d4547d0dc13b9bbd93c8c98276e92d94ee65
Valentin Kuznetsov <vkuznet@gmail.com>
2017-01-13 10:28:19 -0500

    Handle nil data

--- services/phedex.go
+++ services/phedex.go
@@ -37,72 +37,86 @@ func PhedexUnmarshal(api string, data []byte) []mongo.DASRecord {
 				continue
 			}
 
-			val := rec["phedex"].(map[string]interface{})
-			blocks := val["block"].([]interface{})
-			for _, item := range blocks {
-				brec := item.(map[string]interface{})
-				files := brec["file"].([]interface{})
-				for _, elem := range files {
-					frec := elem.(map[string]interface{})
-					out = append(out, frec)
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				blocks := val["block"].([]interface{})
+				for _, item := range blocks {
+					brec := item.(map[string]interface{})
+					files := brec["file"].([]interface{})
+					for _, elem := range files {
+						frec := elem.(map[string]interface{})
+						out = append(out, frec)
+					}
 				}
 			}
 		} else if api == "groups" {
-			val := rec["phedex"].(map[string]interface{})
-			group := val["group"].([]interface{})
-			for _, item := range group {
-				row := item.(map[string]interface{})
-				r := mongo.DASRecord{"name": row["name"].(string)}
-				out = append(out, r)
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				group := val["group"].([]interface{})
+				for _, item := range group {
+					row := item.(map[string]interface{})
+					r := mongo.DASRecord{"name": row["name"].(string)}
+					out = append(out, r)
+				}
 			}
 		} else if api == "blockReplicas" || api == "blockReplicas4dataset" {
-			val := rec["phedex"].(map[string]interface{})
-			blocks := val["block"].([]interface{})
-			for _, item := range blocks {
-				brec := item.(map[string]interface{})
-				out = append(out, brec)
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				blocks := val["block"].([]interface{})
+				for _, item := range blocks {
+					brec := item.(map[string]interface{})
+					out = append(out, brec)
+				}
 			}
 		} else if api == "site4dataset" {
-			val := rec["phedex"].(map[string]interface{})
-			blocks := val["block"].([]interface{})
-			for _, item := range blocks {
-				brec := item.(map[string]interface{})
-				replicas := brec["replica"].([]interface{})
-				for _, val := range replicas {
-					row := val.(map[string]interface{})
-					node := row["node"].(string)
-					se := row["se"].(string)
-					rec := mongo.DASRecord{"name": node, "se": se}
-					out = append(out, rec)
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				blocks := val["block"].([]interface{})
+				for _, item := range blocks {
+					brec := item.(map[string]interface{})
+					replicas := brec["replica"].([]interface{})
+					for _, val := range replicas {
+						row := val.(map[string]interface{})
+						node := row["node"].(string)
+						se := row["se"].(string)
+						rec := mongo.DASRecord{"name": node, "se": se}
+						out = append(out, rec)
+					}
 				}
 			}
 		} else if api == "dataset4site" || api == "dataset4site_group" || api == "dataset4se" || api == "dataset4se_group" {
-			val := rec["phedex"].(map[string]interface{})
-			blocks := val["block"].([]interface{})
-			for _, item := range blocks {
-				brec := item.(map[string]interface{})
-				dataset := strings.Split(brec["name"].(string), "#")[0]
-				rec = mongo.DASRecord{"name": dataset}
-				out = append(out, rec)
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				blocks := val["block"].([]interface{})
+				for _, item := range blocks {
+					brec := item.(map[string]interface{})
+					dataset := strings.Split(brec["name"].(string), "#")[0]
+					rec = mongo.DASRecord{"name": dataset}
+					out = append(out, rec)
+				}
 			}
 		} else if api == "block4site" || api == "block4se" {
-			val := rec["phedex"].(map[string]interface{})
-			blocks := val["block"].([]interface{})
-			for _, item := range blocks {
-				brec := item.(map[string]interface{})
-				rec = mongo.DASRecord{"name": brec["name"].(string)}
-				out = append(out, rec)
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				blocks := val["block"].([]interface{})
+				for _, item := range blocks {
+					brec := item.(map[string]interface{})
+					rec = mongo.DASRecord{"name": brec["name"].(string)}
+					out = append(out, rec)
+				}
 			}
 		} else if api == "nodeusage" || api == "groupusage" || api == "nodes" {
-			val := rec["phedex"].(map[string]interface{})
-			groups := val["node"].([]interface{})
-			for _, item := range groups {
-				brec := item.(map[string]interface{})
-				prec := make(mongo.DASRecord)
-				for k, v := range brec {
-					prec[k] = v
+			if rec["phedex"] != nil {
+				val := rec["phedex"].(map[string]interface{})
+				groups := val["node"].([]interface{})
+				for _, item := range groups {
+					brec := item.(map[string]interface{})
+					prec := make(mongo.DASRecord)
+					for k, v := range brec {
+						prec[k] = v
+					}
+					out = append(out, prec)
 				}
-				out = append(out, prec)
 			}
 		} else {
 			out = append(out, rec)
