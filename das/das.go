d6824cb583ee8a5a1dc8129b7033416ea19f7503
Valentin Kuznetsov <vkuznet@gmail.com>
2018-01-12 15:22:41 -0500

    Fix issue with showing only valid files for file dataset=X queries

--- das/das.go
+++ das/das.go
@@ -80,7 +80,7 @@ func FormUrlCall(dasquery dasql.DASQuery, dasmap mongo.DASRecord) string {
 		}
 		// return only valid files by default
 		if strings.Contains(base, "file") && !utils.InList("status", skeys) {
-			if !strings.Contains(base, "filechildren") && !strings.Contains(base, "fileparents") && !strings.Contains(base, "files") {
+			if !strings.Contains(base, "filechildren") && !strings.Contains(base, "fileparents") {
 				if _, ok := vals["validFileOnly"]; !ok {
 					vals.Add("validFileOnly", "1")
 				}
