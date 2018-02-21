f0d2b45de79fb69daee9b2978885296af208613c
Valentin Kuznetsov <vkuznet@gmail.com>
2018-02-21 09:37:09 -0500

    Added ServerKey/ServerCrt

--- config/config.go
+++ config/config.go
@@ -31,6 +31,8 @@ type Configuration struct {
 	Views         []string `json:"views"`         // list of supported views
 	Verbose       int      `json:"verbose"`       // verbosity level
 	DasMaps       string   `json:"dasmaps"`       // location of dasmaps
+	ServerKey     string   `json:"serverkey"`     // server key for https
+	ServerCrt     string   `json:"servercrt"`     // server certificate for https
 }
 
 // global variables
@@ -38,7 +40,7 @@ var Config Configuration
 
 // String returns string representation of DAS Config
 func (c *Configuration) String() string {
-	return fmt.Sprintf("<Config port=%d uri=%s services=%v queueLimit=%d retry=%d templates=%s js=%s images=%s css=%s yui=%s hkey=%s base=%s dbs=%v views=%v>", c.Port, c.Uri, c.Services, c.UrlQueueLimit, c.UrlRetry, c.Templates, c.Jscripts, c.Images, c.Styles, c.YuiRoot, c.Hkey, c.Base, c.DbsInstances, c.Views)
+	return fmt.Sprintf("<Config port=%d uri=%s services=%v queueLimit=%d retry=%d templates=%s js=%s images=%s css=%s yui=%s hkey=%s base=%s dbs=%v views=%v maps=%v crt=%s key=%s>", c.Port, c.Uri, c.Services, c.UrlQueueLimit, c.UrlRetry, c.Templates, c.Jscripts, c.Images, c.Styles, c.YuiRoot, c.Hkey, c.Base, c.DbsInstances, c.Views, c.DasMaps, c.ServerCrt, c.ServerKey)
 }
 
 func ParseConfig(configFile string) error {
