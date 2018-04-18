f2888e77728ee45df8ecc8b0b554e95a20cf28f5
Valentin Kuznetsov <vkuznet@gmail.com>
2018-04-18 13:44:36 -0400

    resolve json issue with mcm4dataset output

--- services/mcm.go
+++ services/mcm.go
@@ -10,14 +10,27 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/dmwm/das2go/mongo"
+	logs "github.com/sirupsen/logrus"
 )
 
 // helper function to load McM data stream
 func loadMcMData(api string, data []byte) []mongo.DASRecord {
 	var out []mongo.DASRecord
 	var rec mongo.DASRecord
+
+	// McM output for https://cms-pdmv.cern.ch/mcm/public/restapi/requests/produces/WToENu_Tune4C_13TeV-pythia8/Fall13dr-tsg_PU40bx25_POSTLS162_V2-v1/GEN-SIM-RAW
+	// returns formatted JSON with \n. We convert bytes back to string
+	// and replace/trim unnecessary stuff
+	if api == "mcm4dataset" {
+		r := strings.Replace(string(data), "\\n", "", -1)
+		r = strings.Replace(r, " ", "", -1)
+		r = strings.Replace(r, "\\", "", -1)
+		r = strings.Trim(r, "\"")
+		data = []byte(r)
+	}
 
 	// to prevent json.Unmarshal behavior to convert all numbers to float
 	// we'll use json decode method with instructions to use numbers as is
@@ -27,10 +40,16 @@ func loadMcMData(api string, data []byte) []mongo.DASRecord {
 	err := dec.Decode(&rec)
 
 	// original way to decode data
-	// err := json.Unmarshal(data, &rec)
+	//     err := json.Unmarshal(data, &rec)
 	if err != nil {
 		msg := fmt.Sprintf("McM unable to unmarshal the data into DAS record, api=%s, data=%s, error=%v", api, string(data), err)
+		logs.WithFields(logs.Fields{
+			"Error": err,
+			"Api":   api,
+			"data":  string(data),
+		}).Error("McM unable to unmarshal the data")
 		out = append(out, mongo.DASErrorRecord(msg))
+		return out
 	}
 	out = append(out, rec)
 	return out
