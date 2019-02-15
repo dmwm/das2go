8a5a6975b49065b3201809abdf6d554405789b0d
Valentin Kuznetsov <vkuznet@gmail.com>
2019-02-15 17:02:12 -0500

    Add rules4dataset, rules4block and rules4file APIs based on /dids/{scope}/{name} Rucio API

--- services/rucio.go
+++ services/rucio.go
@@ -22,6 +22,7 @@ import (
 func loadRucioData(api string, data []byte) []mongo.DASRecord {
 	var out []mongo.DASRecord
 
+	fmt.Println("### data", string(data))
 	// Rucio uses application/x-json-stream content type which yields dict records from the server
 	var row []byte
 	for _, r := range data {
@@ -44,6 +45,23 @@ func loadRucioData(api string, data []byte) []mongo.DASRecord {
 		} else {
 			row = append(row, r)
 		}
+	}
+	// last record from Rucio does not have '\n' so we need to collect it
+	if len(row) > 0 {
+		var rec mongo.DASRecord
+		err := json.Unmarshal(row, &rec)
+		if err != nil {
+			msg := fmt.Sprintf("Rucio unable to unmarshal the data into DAS record, api=%s, data=%s, error=%v", api, string(row), err)
+			if utils.VERBOSE > 0 {
+				logs.WithFields(logs.Fields{
+					"Error": err,
+					"Api":   api,
+					"data":  string(row),
+				}).Error("Rucio unable to unmarshal the data")
+			}
+			out = append(out, mongo.DASErrorRecord(msg, utils.RucioErrorName, utils.RucioError))
+		}
+		out = append(out, rec)
 	}
 	return out
 }
@@ -75,6 +93,8 @@ func RucioUnmarshal(dasquery dasql.DASQuery, api string, data []byte) []mongo.DA
 				rec["name"] = k
 				out = append(out, rec)
 			}
+		} else if api == "rules4dataset" || api == "rules4block" || api == "rules4file" {
+			out = append(out, rec)
 		} else if api == "file4dataset_site" || api == "file4block_site" {
 			if val, ok := specs["site"]; ok {
 				site := fmt.Sprintf("%s", val)
